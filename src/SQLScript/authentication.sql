ba2ac17b7459900aced9fe66ced137a4b9bd7f10
Nyfeu <andre.maiolini@hotmail.com>
2024-08-13 10:30:34 -0300

    Adicionando relação notas e faltas ao DB

--- src/SQLScript/authentication.sql
+++ src/SQLScript/authentication.sql
@@ -1,8 +1,20 @@
 create database login_senha;
+
 use login_senha;
+
 create table usuario(
     nome VARCHAR(60) NOT NULL,
     email VARCHAR(60) NOT NULL,
     senha_hash VARCHAR(128) NOT NULL,
     primary key(email)
+);
+
+create table notas(
+    email VARCHAR(60) NOT NULL,
+    nota FLOAT(4, 2) UNSIGNED
+);
+
+create table faltas(
+    email VARCHAR(60) NOT NULL,
+    quantidade SMALLINT UNSIGNED NOT NULL
 );
